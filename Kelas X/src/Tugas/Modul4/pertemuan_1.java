989e8bd333acae81b14cc74b0763a256c069d230
ValenNz <92833376+ValenVz@users.noreply.github.com>
2021-11-25 05:35:54 +0700

    Add files via upload

---
+++ Kelas X/src/Tugas/Modul4/pertemuan_1.java
@@ -0,0 +1,23 @@
+package Tugas.Modul4;
+
+public class pertemuan_1 {
+    public static void main(String[] args) {
+        //01
+        String nama,alamat, kelas;
+        int usia;
+        nama = "Bagus";
+        kelas = "X RPL 1";
+        usia = 16;
+
+        System.out.println("nama: "+ nama);
+
+        // 05
+        System.out.println("Nuevalen Refitra Alswando\nXR7\nSMK TELKOM MALANG");
+
+        // 06
+        System.out.println("nama   :  bagus");
+        System.out.println("Alamat : SMK Telkom Malang");
+        System.out.println("Kelas  : X RPL 1");
+
+    }
+}
---
+++ Kelas X/src/Tugas/Modul4/pertemuan_2.java
@@ -0,0 +1,21 @@
+package Tugas.Modul4;
+
+public class pertemuan_2 {
+    public static void main(String[] args) {
+        // 02
+        String nama,kelas;
+        Double p,l,luas;
+
+        nama = "Anfif";
+        kelas = "X RPL 1";
+        p=10.0;
+        l=7.5;
+
+        luas = p * l;
+
+        System.out.println("nama: "+ nama);
+        System.out.println("kelas: "+ kelas);
+        System.out.print("Luas persegi :"+ luas +" m bisa digunakan untuk sepak bola ");
+
+    }
+}

91add2e03b98b2c6d6d43ded81e997cc33cb8c41
ValenNz <92833376+ValenNz@users.noreply.github.com>
2022-04-27 21:54:13 +0700

    Delete Kelas X/src/UKL_XR7_28_smt2 directory

--- Kelas X/src/UKL_XR7_28_smt2/Pepus/subclass.java
+++
@@ -1,7 +0,0 @@
-package UKL_XR7_28_smt2.Pepus;
-
-public class subclass extends superclass{
-    subclass(){
-        super("pesan");
-    }
-}
--- Kelas X/src/UKL_XR7_28_smt2/Pepus/superclass.java
+++
@@ -1,8 +0,0 @@
-package UKL_XR7_28_smt2.Pepus;
-
-public class superclass {
-    String pesan = "Pesan Default ini Ditambah : ";
-    superclass(String param){
-        System.out.println(this.pesan + param);
-    }
-}
